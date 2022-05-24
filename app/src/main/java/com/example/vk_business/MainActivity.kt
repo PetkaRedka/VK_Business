54e8bfeb94400611060f4dbce9d52dc0cdffb348
peter <zhiliaev.petr@mail.ru>
2022-05-24 14:42:49 +0300

    created planer view

--- app/src/main/java/com/example/vk_business/MainActivity.kt
+++ app/src/main/java/com/example/vk_business/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.vk_business
 
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.view.isVisible
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -13,7 +15,6 @@ class MainActivity: LogginActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.main_window)
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
