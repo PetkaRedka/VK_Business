79b2d14d3ebeb5a40d3bfb7c73c0de3138a1142e
Peter <zhiliaev.petr@mail.ru>
2022-05-26 22:02:41 +0300

    change to neon design (login), change app icon

--- app/src/main/java/com/example/vk_business/LogginActivity.kt
+++ app/src/main/java/com/example/vk_business/LogginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.vk_business
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.smb.glowbutton.GlowButton
 
 
 open class LogginActivity : AppCompatActivity() {
@@ -12,9 +15,16 @@ open class LogginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.loggin_view)
 
+        // Убираем верхнюю полоску
         supportActionBar?.hide()
+        // Делаем приложение на весь экран
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Убираем кнопки снизу
+        if (Build.VERSION.SDK_INT >= 19) {window.decorView.systemUiVisibility =
+            (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)}
 
-        val loggin_button: Button = findViewById(R.id.sign_in_button)
+        val loggin_button: GlowButton = findViewById(R.id.sign_in_button)
         loggin_button.setOnClickListener {
             val intent = Intent(this@LogginActivity, MainActivity::class.java)
             startActivity(intent)
